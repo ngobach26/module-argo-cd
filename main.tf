60a34d82951be1bd5515a46247eb2f609c35e72d
ngobach26 <ngobach26@gmail.com>
2025-09-02 21:25:19 +0700

    fix

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-provider "kubernetes" {
-  load_config_file       = false
-  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
-  host                   = var.kubernetes_cluster_endpoint
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws-iam-authenticator"
-    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    load_config_file       = false
-    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
-    host                   = var.kubernetes_cluster_endpoint
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      command     = "aws-iam-authenticator"
-      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
-    }
-  }
-}
-
-
 /*
 provider "aws" {
   region = var.aws_region
@@ -33,7 +8,6 @@ data "aws_eks_cluster" "msur" {
 }
 
 provider "kubernetes" {
-  load_config_file       = false
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
   host                   = data.aws_eks_cluster.msur.endpoint
   exec {
@@ -47,7 +21,6 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    load_config_file       = false
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
     host                   = data.aws_eks_cluster.msur.endpoint
     exec {
